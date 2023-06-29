ea7cb84bc9ba3c7b6c07640df947c2abe3712992
Mauro Rocha <126111579+NeuronioAzul@users.noreply.github.com>
2023-06-29 16:47:19 -0300

    config(Route): Web resource route to images

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('images')->group(function () {
+    Route::resource('/', ImageController::class);
 });
